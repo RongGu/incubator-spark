1889d0a6aa0568f03e792c5e58a6b68d5f925a75
RongGu <gurongwalker@gmail.com>
2014-02-11 00:46:27 +0800

    update the dependent tachyon version to 0.4 and downgrade the jvm version to 1.6

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -40,8 +40,8 @@ object SparkBuild extends Build {
   val HBASE_VERSION = "0.94.6"
 
   // Target JVM version
-  val SCALAC_JVM_VERSION = "jvm-1.7"
-  val JAVAC_JVM_VERSION = "1.7"
+  val SCALAC_JVM_VERSION = "jvm-1.6"
+  val JAVAC_JVM_VERSION = "1.6"
     
   lazy val root = Project("root", file("."), settings = rootSettings) aggregate(allProjects: _*)
 
@@ -282,7 +282,7 @@ object SparkBuild extends Build {
         "com.twitter"             %% "chill"            % "0.3.1",
         "com.twitter"              % "chill-java"       % "0.3.1",
         "com.typesafe"             % "config"           % "1.0.2",
-        "org.tachyonproject"	   % "tachyon" 		% "0.3.0" excludeAll(excludeKyro, excludeHadoop, excludeCurator, excludeJackson, excludeNetty, excludeAsm),
+        "org.tachyonproject"	   % "tachyon" 		% "0.4.0" excludeAll(excludeKyro, excludeHadoop, excludeCurator, excludeJackson, excludeNetty, excludeAsm),
         "com.clearspring.analytics" % "stream"          % "2.5.1"
       )
   )
